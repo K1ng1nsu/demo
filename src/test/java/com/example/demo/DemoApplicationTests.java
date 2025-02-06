afcdaf135e7567b9caec28128aaaff414cd2d0f9
K1ng1nsu <96197161+K1ng1nsu@users.noreply.github.com>
2025-02-06 20:25:35 +0900

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,14 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int result = 5;
+		assertEquals(5, result, "2 + 3은 5여야 합니다.");
+	}
+
+	@Test
+	public void testAdd() {
+		int result = 2 + 3;
+		assertEquals(4, result, "2 + 3은 5여야 합니다. 의도적 오류 발생");
 	}
 
 }
